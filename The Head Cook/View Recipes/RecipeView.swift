c90ed66f3a6ce94066509d04d30c446a13185980
dard2002 <dylan.5software@gmail.com>
2024-09-09 21:25:17 +1000

    Updated RecipeView with better identation, comments and moved recipe nil check further up

--- The Head Cook/View Recipes/RecipeView.swift
+++ The Head Cook/View Recipes/RecipeView.swift
@@ -11,10 +11,12 @@ struct RecipeView: View {
     @Binding var recipe: Recipe?
     
     var body: some View {
-            ScrollView {
-                VStack {
-                    Text("\(recipe!.name)").font(.largeTitle).bold()
-                    if let imageData = recipe!.image, let uiImage = UIImage(data: imageData) {
+        ScrollView {
+            VStack {
+                // Check for a nil recipe, if it is nil display the Text "No Recipe Found!".
+                if let recipe = recipe {
+                    Text("\(recipe.name)").font(.largeTitle).bold()
+                    if let imageData = recipe.image, let uiImage = UIImage(data: imageData) {
                         Image(uiImage: uiImage)
                             .resizable()
                             .scaledToFit()
@@ -28,33 +30,35 @@ struct RecipeView: View {
                     }
                     
                     Text("Ingredients:").font(.title2).bold()
-                    if let recipe = recipe {
-                        ForEach(recipe.ingredients.sorted(by: { $0.name < $1.name } ), id: \.id) { ingredient in
-                            HStack {
-                                if let imageData = ingredient.image, let uiImage = UIImage(data: imageData) {
-                                    Image(uiImage: uiImage)
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 150, height: 150)
-                                } else {
-                                    // Fallback image or placeholder
-                                    Image(systemName: "photo")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 150, height: 150)
-                                }
-                                
-                                VStack {
-                                    Text("\(ingredient.name)").font(.title3).bold()
-                                    Text("\(ingredient.quantity)")
-                                    Text("\(ingredient.quantityUnits)")
-                                }.frame(maxWidth: (UIScreen.main.bounds.width - 32.0) * 0.8, alignment: .center).padding()
-                                
-                            }.frame(maxWidth: (UIScreen.main.bounds.width - 32.0) * 0.8, alignment: .leading)
-                        }
+                   
+                    ForEach(recipe.ingredients.sorted(by: { $0.name < $1.name } ), id: \.id) { ingredient in
+                        HStack {
+                            if let imageData = ingredient.image, let uiImage = UIImage(data: imageData) {
+                                // Image selected from Recipe when created
+                                Image(uiImage: uiImage)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 150, height: 150)
+                            } else {
+                                // Fallback image or placeholder
+                                Image(systemName: "photo")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 150, height: 150)
+                            }
+                            
+                            // Ingredient details - name, quantity, unit of measurement (quantityUnits) - this is all inside of a VStack
+                            VStack {
+                                Text("\(ingredient.name)").font(.title3).bold()
+                                Text("\(ingredient.quantity)")
+                                Text("\(ingredient.quantityUnits)")
+                            }.frame(maxWidth: (UIScreen.main.bounds.width - 32.0) * 0.8, alignment: .center).padding()
+                            
+                        }.frame(maxWidth: (UIScreen.main.bounds.width - 32.0) * 0.8, alignment: .leading)
                     }
-                    else {
-                    Text("No Recipe found.")
+                }
+                else {
+                    Text("No Recipe Found!")
                 }
             }
         }
